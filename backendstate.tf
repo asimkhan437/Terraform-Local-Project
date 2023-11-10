3cf08001bd062c543f5c67c17c0b0ee6228f77a9
asimkhan437 <79131166+asimkhan437@users.noreply.github.com>
2023-11-10 13:41:38 +0000

    Delete backendstate.tf

--- backendstate.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-backend"azurerm" {
-    resource_group_name  = "RG-statefile"
-    storage_account_name = "sastatefile"
-    container_name       = "statefile"
-    key                  = "terraform.tfstate"
-    access_key           = "JLO6bFWi2MMX+OZ4EBArKMsuVagvRUJOjFX+LZnGOfFTf+SW/g/d5S7hCsTZUxjcFivmX5RsiEPb+AStC5NB2w=="
-  }
-}
