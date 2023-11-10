4f1dbb524ca516fd60828a5370af69baa8857776
asimkhan437@yahoo.com <asimkhan437@yahoo.com>
2023-11-10 16:28:20 +0000

    c18

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
     }
   }
 
-  #required_version = ">= 1.1.0"
-  required_version = "1.2.3"
+  required_version = ">= 1.1.0"
+  #required_version = "1.2.3"
   #version = "~> 3.0.2"
 }
 provider "azurerm" {
