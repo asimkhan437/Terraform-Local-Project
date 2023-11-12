b2a00f531bbc7d9c3f57d04232cf2c4ce902e359
asimkhan437@yahoo.com <asimkhan437@yahoo.com>
2023-11-12 10:31:02 +0000

    a-3

--- backendstate.tf
+++ backendstate.tf
@@ -5,5 +5,7 @@ terraform {
     container_name       = "statefile"
     key                  = "terraform.tfstate"
     access_key           = "JLO6bFWi2MMX+OZ4EBArKMsuVagvRUJOjFX+LZnGOfFTf+SW/g/d5S7hCsTZUxjcFivmX5RsiEPb+AStC5NB2w=="
+    use-oidc             = true
+    use_azuread_auth     = true
   }
 }
